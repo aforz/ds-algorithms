687cc6f1ecea2257dd248d2a99392ff13695fcec
Afroz Alam <afroz3002@gmail.com>
2025-07-13 13:42:22 +0530

    Added below programs. AllPrimes, PrimesWithout3, FibonacciSequence.

---
+++ com/basics/AllPrimes.java
@@ -0,0 +1,22 @@
+package com.basics;
+
+public class AllPrimes {
+    public static void main(String[] args) {
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+        int L = sc.nextInt();
+        int R = sc.nextInt();
+        for(int i = L; i <= R; i++){
+            if(isPrime(i)){
+                System.out.print(i+ " ");
+            }
+        }
+    }
+
+    public static boolean isPrime(int n){
+        if (n < 2) return false;
+        for (int i = 2; i*i <= n; i++){
+            if(n%i == 0) return false;
+        }
+        return true;
+    }
+}
---
+++ com/basics/FibonacciSequence.java
@@ -0,0 +1,19 @@
+package com.basics;
+
+public class FibonacciSequence {
+    public static void main(String[] args) {
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+        int num = sc.nextInt();
+        System.out.println("Enter count to create Fibonacci sequence: " + num);
+        int a = 0;
+        int b = 1;
+        System.out.print(a+ " ");
+        System.out.print(b+ " ");
+        for(int i = 3; i <= num; i++){
+            int nextItem = a + b;
+            System.out.print(nextItem + " ");
+            a = b;
+            b = nextItem;
+        }
+    }
+}
